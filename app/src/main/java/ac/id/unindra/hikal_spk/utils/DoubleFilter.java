8d5226e599a1015b09bb1087626b379f70b72828
Ripaldiansyah <ripaldiansyah13@gmail.com>
2024-06-05 20:52:39 +0700

    CHANGE : int filter to double filter

--- app/src/main/java/ac/id/unindra/hikal_spk/utils/DoubleFilter.java
+++ app/src/main/java/ac/id/unindra/hikal_spk/utils/DoubleFilter.java
@@ -4,14 +4,14 @@ import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
 
-public class IntFilter extends DocumentFilter {
+public class DoubleFilter extends DocumentFilter {
     @Override
     public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr)
             throws BadLocationException {
         if (string == null) {
             return;
         }
-        if (string.matches("\\d+")) {
+        if (isValidDouble(fb.getDocument().getText(0, fb.getDocument().getLength()), offset, string)) {
             super.insertString(fb, offset, string, attr);
         }
     }
@@ -22,7 +22,7 @@ public class IntFilter extends DocumentFilter {
         if (text == null) {
             return;
         }
-        if (text.matches("\\d+")) {
+        if (isValidDouble(fb.getDocument().getText(0, fb.getDocument().getLength()), offset, text)) {
             super.replace(fb, offset, length, text, attrs);
         }
     }
@@ -30,5 +30,20 @@ public class IntFilter extends DocumentFilter {
     @Override
     public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
         super.remove(fb, offset, length);
+    }
+
+    private boolean isValidDouble(String currentText, int offset, String newText) {
+        try {
+            StringBuilder sb = new StringBuilder(currentText);
+            sb.insert(offset, newText);
+            String finalText = sb.toString();
+
+            if (finalText.matches("-?\\d*(\\.\\d*)?")) {
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return false;
     }
 }
